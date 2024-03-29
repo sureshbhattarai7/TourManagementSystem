0de89820da3487fcc55169aa088560096336446a
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-29 21:40:38 +0545

    Add Tour URL

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: "./config.env" });
 
+const tourRoute = require('./Router/tourRoute');
+
 const app = express();
 app.use(express.json());
+
+app.use('/api/v1/tours', tourRoute);
 
 module.exports = app;
