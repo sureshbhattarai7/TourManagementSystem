2259de67c72f23fcfb9d627af6ecf82c1a1d9008
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-29 21:38:52 +0545

    Add discount price

--- Model/tourModel.js
+++ Model/tourModel.js
@@ -55,7 +55,7 @@ const tourSchema = mongoose.Schema({
     },
     imageCover: {
         type: String,
-        required: [true, 'A tour must have a cover image']
+        //required: [true, 'A tour must have a cover image']
     },
     images: [String],
     createdAt: {
