fffd85725ca89a584bbcad75d641694f4db6b1ac
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-30 20:44:19 +0545

    Implement Post Route

--- Router/tourRoute.js
+++ Router/tourRoute.js
@@ -5,7 +5,9 @@ const tourController = require('./../Controller/tourController');
 router.route('/')
     .post(tourController.createTour)
     .get(tourController.getTour);
-    
-router.route('/:id').get(tourController.getTour)
+
+router.route('/:id')
+    .get(tourController.getTour)
+    .post(tourController.updateTour);
 
 module.exports = router;
