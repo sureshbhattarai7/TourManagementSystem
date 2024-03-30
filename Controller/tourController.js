fa4a3bd723a36d2f319f41bbf122a4b96fefb8a4
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-30 20:36:51 +0545

    Implement Get Method

--- Controller/tourController.js
+++ Controller/tourController.js
@@ -1,18 +1,37 @@
 const Tour = require('./../Model/tourModel');
+const AppError = require('./../Utils/appError');
+const catchasync = require('./../Utils/catchAsync');
 
-exports.createTour = async (req, res, next) => {
-    try {
-        const tour = await Tour.create(req.body);
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        });
-    };
-};
+exports.createTour = catchasync(async (req, res, next) => {
+    const tour = await Tour.create(req.body);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
+
+exports.getTours = catchasync(async (req, res, next) => {
+    const tour = await Tour.find();
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+});
+
+exports.getTour = catchasync(async (req, res, next) => {
+    const tour = await Tour.findById(req.params.id);
+    if (!tour) {
+        return new AppError(`No Tour found with that ID!`, 404);
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+});
+
