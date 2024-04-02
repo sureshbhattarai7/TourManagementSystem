2b08761ea8f7b105417b9ac10350a27e3e00fea7
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-04-02 20:39:46 +0545

    Implement user route middleware

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: "./config.env" });
 
 const tourRoute = require('./Router/tourRoute');
+const userRoute = require('./Router/userRoute');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/users', userRoute);
 
 module.exports = app;
