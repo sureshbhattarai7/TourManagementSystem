91a7b5578d4b2ae7788e3039e2da74982a235aff
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-04-03 20:42:11 +0545

    Implement login route

--- Router/userRoute.js
+++ Router/userRoute.js
@@ -3,3 +3,6 @@ const router = express.Router();
 const userController = require('./../Controller/userController');
 
 router.route('/signup').post(userController.signup);
+router.route('/login').post(userController.login);
+
+module.exports = router;
